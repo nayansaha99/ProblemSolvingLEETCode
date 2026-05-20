3eef2a6f154bdabe65a67314e319c796620b7bdc
Your Name <you@example.com>
2026-05-20 08:25:37 -0700

    first commit

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ var removeDuplicates = function (arr) {
     let j = 1; //pointer
     
     for (let i = 1; i < arr.length; i++) {
-        if (arr[i] !== arr[i - 1]) {     //condition
-            arr[j] = arr[i];
-          
-             j++;
-            console.log("arr:",i,j)
-            
+        if (arr[i] !== arr[i - 1]) { 
+            arr[j] = arr[i]
+            console.log("arr",arr[j])
+            j++;
+            console.log("arr",arr[j])
         }
     }
+//    console.log(arr[j]);
+//    arr.length = j;
    return j;
-   console.log("arr",j)
     
 }
 
