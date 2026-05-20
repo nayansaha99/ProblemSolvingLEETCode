6cfb6dca0e54a1b73fcf024964b31a5ca89d3d6c
Your Name <you@example.com>
2026-05-19 17:19:39 -0700

    first commit

--- index.js
+++ index.js
@@ -1,33 +1,26 @@
 //26.Remove_Duplicates_from_Sorted_Array.js//
-const arr = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4,4,5,5,6,6,7,7,8,8,9,9,10,10]
-function  removeDuplicates(arr) {
-     if(arr.length===0) return[];
-     let k = 1;
-     for(let i=0;i<arr.length;i++){
-       
-        if(arr[i]!==arr[i-1]){
-            arr[k]==arr[i]
-           
-            k++;
+let arr = [5, 6, 7, 7, 8, 8, 9]
+var removeDuplicates = function (arr) {
+    if (arr.length === 0) return [];
+    let j = 1; //pointer
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i - 1]) {     //condition
+            arr[j] = arr[i];
+          
+             j++;
+            console.log("arr:",i,j)
+            
         }
-
-     }
-     return k;
-
-
     }
-    let result=removeDuplicates(arr);
-    console.log(result)
+   return j;
+   console.log("arr",j)
+    
+}
 
-    // if(arr.length===0) return [];
-    // let k=[];
-    // for(let i=0; i<arr.length;i++){
-    //     if(arr[i]!==arr[i-1]){
-    //         arr[k]==arr[i];
-    //         k++;
-    //     }
-    // }
-    // return k;
+console.log(removeDuplicates(arr))
+
+
 
 
 
